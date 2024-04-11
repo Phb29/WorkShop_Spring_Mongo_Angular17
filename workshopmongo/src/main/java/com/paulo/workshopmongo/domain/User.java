51cde724cac3ffb152da243a5bf081d6e915a257
paulo henique <paulohenriquephb29@gmail.com>
2024-04-11 00:30:39 -0300

    finddAll

--- workshopmongo/src/main/java/com/paulo/workshopmongo/domain/User.java
+++ workshopmongo/src/main/java/com/paulo/workshopmongo/domain/User.java
@@ -3,16 +3,19 @@ package com.paulo.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document(collection = "user")
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+	@Id
 	private String id;
 	private String name;
 	private String email;
-	
-	
-	public  User() {}
 
+	public User() {
+	}
 
 	public User(String id, String name, String email) {
 		super();
@@ -21,42 +24,34 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
-
 	public String getId() {
 		return id;
 	}
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -69,9 +64,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
 
 }
--- workshopmongo/src/main/java/com/paulo/workshopmongo/resources/UserResources.java
+++ workshopmongo/src/main/java/com/paulo/workshopmongo/resources/UserResources.java
@@ -1,27 +1,27 @@
 package com.paulo.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paulo.workshopmongo.domain.User;
+import com.paulo.workshopmongo.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-	
+
+	@Autowired
+	private UserService service;
+
 	@RequestMapping(method = RequestMethod.GET)
-public ResponseEntity<List<User>>  findAll(){
-	
-	User maria = new User("1","maria","paulo@");
-	
-	List<User> list =new ArrayList<>();
-	list.addAll(Arrays.asList(maria));
-	return ResponseEntity.ok().body(list);
-}
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 }
